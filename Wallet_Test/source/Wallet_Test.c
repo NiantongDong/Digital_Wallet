a17d30c87c647bd15b9704bc511222c6728f5d17
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-04-03 11:49:09 -0400

    Hardcoded Keys
    WIP for getting keys from file. Key files don't seem to be transferring to board?

--- Wallet_Test/source/Wallet_Test.c
+++ Wallet_Test/source/Wallet_Test.c
@@ -70,14 +70,52 @@ int main(void) {
     int bufferSize = 20;
     char *inputBuffer = malloc(bufferSize * sizeof(char));
 
-    //Flags~~~
+    //Flags
     int entranceFlag = 0;
     int bodyFlag = 1;
 
     /* Keys and file management*/
     char output_buffer[128];
 
+    char *public_key =
+    		"-----BEGIN PUBLIC KEY-----"
+    		"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv6Dmm3NvbVDz2Nh+wwc+"
+    		"YOjGLDSuSu940LMiyIkfOXRPd2AhUjV44MW98eVlDrG885dqBG/Hr0JVKufXAAEi"
+    		"4atyf3epluU9gcWbdEHA9dPwnNntMb4L4H8TPMju91nrTHE5uI+Sz9QtwaCTF9Ll"
+    		"FotM0+PT+0YVLDjsb8phVg9nPyHUrror6Uy9blAl7JMfUg6T6kflqPZoQuA6L2uk"
+    		"T7XO1v+5k8B4g7sRIbwtBfwD7cYZDLDMXFSqJKn9Ilg6lzHKUdQc+wn7WlSqDANf"
+    		"qHec4Ph4AxExW4/V7u80XI/mcT+6c0rgna58AvOwPCtzMVAd2hu0eY3DIhhXNYC5"
+    		"NwIDAQAB"
+    		"-----END PUBLIC KEY-----";
 
+    char *private_key =
+    		"-----BEGIN RSA PRIVATE KEY-----"
+    		"MIIEowIBAAKCAQEAv6Dmm3NvbVDz2Nh+wwc+YOjGLDSuSu940LMiyIkfOXRPd2Ah"
+    		"UjV44MW98eVlDrG885dqBG/Hr0JVKufXAAEi4atyf3epluU9gcWbdEHA9dPwnNnt"
+    		"Mb4L4H8TPMju91nrTHE5uI+Sz9QtwaCTF9LlFotM0+PT+0YVLDjsb8phVg9nPyHU"
+    		"rror6Uy9blAl7JMfUg6T6kflqPZoQuA6L2ukT7XO1v+5k8B4g7sRIbwtBfwD7cYZ"
+    		"DLDMXFSqJKn9Ilg6lzHKUdQc+wn7WlSqDANfqHec4Ph4AxExW4/V7u80XI/mcT+6"
+    		"c0rgna58AvOwPCtzMVAd2hu0eY3DIhhXNYC5NwIDAQABAoIBAEZCm2rxtzAwl0ex"
+    		"kUC2vjNQBaEdq64EAZsboyw9wnoRYDWXxH/c5TYcqpkvj48Esg/qO0/nICLtQkbZ"
+    		"/GWfBQgTEYcU+9Q8twGGz4mftj0r+TO5X5N2Z+pnDUmjLqL4kCJV/WDjG2QiGqi8"
+    		"1FHO3HoYlxG1nfqqZobxVBdk761ak6Pzbiw74q77RWxjlBEtgLC2E67eDqJKSUdI"
+    		"VvkFNile+uY99KjQHkiY0Hn5vx1nLSlIKMi5rRsdE+jYgQGI02/fTkWASp/XJixe"
+    		"fNoUGgsTtvPBTJoghqm3A83N11Y8WYv9CPkVBBm6AwHeo6BmJqQ9dzudbHTzaooV"
+    		"ZMPLBQkCgYEA7WvwML+44lxJHlJk+OGw1EsV8T57DM6kVrECN/KsnfQKvBA4vbKI"
+    		"OVApDzRRVLcLEoArXO33EHGwBl6flOvMM8VSzSEkPBc6BF7I9dMvWDOGJIrKs6Kl"
+    		"YLq75ay6/+zhot9peohvdsXsWHWtG49h72SS47DSBmVsvHQFxsIJ2cUCgYEAzp+g"
+    		"+J/LH9JDqDt5pO6n7tksTVhv4HH9JCTZ3Jf2RgIfIgUKIsTBUUtsUwQC/aX4Cwuu"
+    		"l8KFzyuTdzAziEUaEu+3Dpx3KAYb/XcZ83pp2iTyTP3zuK9VGnIgBbpZvJSL0pJq"
+    		"qweGzJBH5mvxmc9GnzUyzt22pW81qvz0M9tfgssCgYBtZ+fvDuorpUssZduRtK6/"
+    		"pTofSSN6615fdIIJNhLFVsf7kZ4UDEkp3/6LuxgfaZn4lIdm82F+emUbCk+PWHlp"
+    		"Aez+Cd+gsTNTADqqzP5sLpIKFMsDrFd5E5Oyar5hYM85NqpshKKYoOwgd1ml8ZS3"
+    		"tCjcM4cU23gX/38J8DCJ4QKBgD5f4fAsDN2mU9ZNXgN0KC3qUX1n3R4cSxbGKR52"
+    		"oVmw+kUC0HR+gP1Rj3ngFCcOGzr6Y9JcXpiTxwiL/IJ+mchtpQ08Hpxue7SgZ60g"
+    		"nyPDU4H6h8CQ7Vf2a8RO5RgQn8UJfjnzI7ZNN+dPXrfX4rHdFWFtfN2kHMdpZS7i"
+    		"YorRAoGBAOzJGmaIAgZoy4dl1wW7cQhtFHUQjn/AXhk3r6HZryfbqTQ2UjIzB9Cp"
+    		"4J5cMvACdleZaeDF+UpW1iu2E6aqGD9/zARQrTKrR2L/AHETqQHwKUtubO2b9G2q"
+    		"p+gYN2bV4VO9iL1H0TqqUGy/DK9iPAgigeP7/fy+Xa9v+Wy1EhzY"
+    		"-----END RSA PRIVATE KEY-----";
 
 
     while(entranceFlag == 0 )
@@ -127,9 +165,9 @@ int main(void) {
 		{
 			PRINTF("\nEntered public key. \n\n");
 		    //FILE *fp_public = fopen("49742dfdf5-public.pem.key", "r");
-		    FILE *fp_public = fopen("C:/Users/bkute/Documents/MCUXpressoIDE_11.3.0_5222/workspace/Wallet_Test/source/test.txt", "r");
+		    FILE *fp_public = fopen("./test.txt", "r");
 		     if(fp_public == NULL) {
-		    	 PRINTF("Unable to open file!\n\n");
+		    	 PRINTF("Unable to open file!\nUsing hard-coded value for now:\n%s\n\n",public_key);
 		     }
 		     else
 		     {
@@ -143,9 +181,9 @@ int main(void) {
 		{
 			PRINTF("\nEntered private key. \n\n");
 		    //FILE *fp_private = fopen("49742dfdf5-private.pem.key", "r");
-		    FILE *fp_private = fopen("C:/Users/bkute/Documents/MCUXpressoIDE_11.3.0_5222/workspace/Wallet_Test/source/test.txt", "r");
+		    FILE *fp_private = fopen("./test.txt", "r");
 		    if(fp_private == NULL) {
-		    	 PRINTF("Unable to open file!\n\n");
+		    	 PRINTF("Unable to open file!\nUsing hard-coded value for now:\n%s\n\n",private_key);
 		    }
 		    else
 		    {
