953cc41cb679215c403003d20832b41870a76ba5
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-04-07 23:02:54 -0400

    Adding code with working parsing from private key
    Code currently compiles and runs, but outputs binary. Needs to be converted to base64 before being compared to decrpytion via our public key.

---
+++ frdmk64f_mbedtls_benchmark/Debug/component/uart/fsl_adapter_uart.d
@@ -0,0 +1,41 @@
+component/uart/fsl_adapter_uart.o component/uart/fsl_adapter_uart.d: \
+ ../component/uart/fsl_adapter_uart.c \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/fsl_device_registers.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/core_cm4.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_version.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_compiler.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_gcc.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/system_MK64F12.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12_features.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_clock.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_uart.h \
+ ../component/uart/fsl_adapter_uart.h
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/fsl_device_registers.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/core_cm4.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_version.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_compiler.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_gcc.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/system_MK64F12.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12_features.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_clock.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_uart.h:
+
+../component/uart/fsl_adapter_uart.h:
---
+++ frdmk64f_mbedtls_benchmark/Debug/utilities/fsl_debug_console.d
@@ -0,0 +1,52 @@
+utilities/fsl_debug_console.o utilities/fsl_debug_console.d: \
+ ../utilities/fsl_debug_console.c ../utilities/fsl_debug_console_conf.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/fsl_device_registers.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/core_cm4.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_version.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_compiler.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_gcc.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/system_MK64F12.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12_features.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_clock.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h \
+ ../utilities/fsl_str.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\serial_manager/fsl_component_serial_manager.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\serial_manager/fsl_component_serial_port_uart.h \
+ C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\uart/fsl_adapter_uart.h \
+ ../utilities/fsl_debug_console.h
+
+../utilities/fsl_debug_console_conf.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/fsl_device_registers.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/core_cm4.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_version.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_compiler.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\CMSIS/cmsis_gcc.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/system_MK64F12.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\device/MK64F12_features.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_clock.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\drivers/fsl_common.h:
+
+../utilities/fsl_str.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\serial_manager/fsl_component_serial_manager.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\serial_manager/fsl_component_serial_port_uart.h:
+
+C:\Users\bkute\Documents\MCUXpressoIDE_11.3.0_5222\workspace\frdmk64f_mbedtls_benchmark\component\uart/fsl_adapter_uart.h:
+
+../utilities/fsl_debug_console.h:
