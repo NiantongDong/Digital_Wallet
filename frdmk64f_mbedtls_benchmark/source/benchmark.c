9cca01ef7bf1ad30a9f975bebf51bbbf47d70caf
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-04-17 18:51:44 -0400

    Pruning code
    Pruning some of the preexisting code, since we kinda latched onto an example to get the dependencies necessary for the mbedtls libraries.

--- frdmk64f_mbedtls_benchmark/source/benchmark.c
+++ frdmk64f_mbedtls_benchmark/source/benchmark.c
@@ -152,13 +152,6 @@ int main(void)
 #define HEADER_FORMAT   "  %-24s :  "
 #define TITLE_LEN       25
 
-#define OPTIONS                                                         \
-    "md4, md5, ripemd160, sha1, sha256, sha512,\n"                      \
-    "arc4, des3, des, camellia, blowfish, chacha20,\n"                  \
-    "aes_cbc, aes_gcm, aes_ccm, aes_ctx, chachapoly,\n"                 \
-    "aes_cmac, des3_cmac, poly1305\n"                                   \
-    "havege, ctr_drbg, hmac_drbg\n"                                     \
-    "rsa, dhm, ecdsa, ecdh.\n"
 
 #if defined(MBEDTLS_ERROR_C)
 #define PRINT_ERROR                                                     \
@@ -169,41 +162,7 @@ int main(void)
         mbedtls_printf( "FAILED: -0x%04x\n", -ret );
 #endif
 
-#define TIME_AND_TSC(TITLE, CODE)                                                                        \
-    do                                                                                                   \
-    {                                                                                                    \
-        uint32_t ii, jj;                                                                                 \
-        uint64_t tsc1, tsc2;                                                                             \
-        int ret = 0;                                                                                     \
-                                                                                                         \
-        mbedtls_printf(HEADER_FORMAT, TITLE);                                                            \
-        fflush(stdout);                                                                                  \
-                                                                                                         \
-        benchmark_mbedtls_set_alarm(1);                                                                  \
-        tsc1 = benchmark_mbedtls_timing_hardclock();                                                     \
-        for (ii = 1; ret == 0 && !benchmark_mbedtls_timing_alarmed; ii++)                                \
-        {                                                                                                \
-            ret = CODE;                                                                                  \
-            benchmark_mbedtls_poll_alarm();                                                              \
-        }                                                                                                \
-                                                                                                         \
-        tsc2 = benchmark_mbedtls_timing_hardclock();                                                     \
-        for (jj = 0; ret == 0 && jj < 1024; jj++)                                                        \
-        {                                                                                                \
-            ret = CODE;                                                                                  \
-        }                                                                                                \
-                                                                                                         \
-        if (ret != 0)                                                                                    \
-        {                                                                                                \
-            PRINT_ERROR;                                                                                 \
-        }                                                                                                \
-        else                                                                                             \
-        {                                                                                                \
-            mbedtls_printf("%6.2f KB/s,  %6.2f cycles/byte\r\n",                                         \
-                           (ii * signSIZE / 1024) / (((float)(tsc2 - tsc1)) / CLOCK_GetCoreSysClkFreq()), \
-                           (((float)(benchmark_mbedtls_timing_hardclock() - tsc2)) / (jj * signSIZE)));   \
-        }                                                                                                \
-    } while (0)
+
 
 #if defined(MBEDTLS_MEMORY_signFER_ALLOC_C) && defined(MBEDTLS_MEMORY_DEBUG)
 
@@ -226,46 +185,7 @@ int main(void)
 #define MEMORY_MEASURE_PRINT( title_len )
 #endif
 
-#define TIME_PUBLIC(TITLE, TYPE, CODE)                                                                                \
-    do                                                                                                                \
-    {                                                                                                                 \
-        uint32_t ii;                                                                                                  \
-        uint64_t tsc;                                                                                                 \
-        int ret;                                                                                                      \
-        MEMORY_MEASURE_INIT;                                                                                          \
-                                                                                                                      \
-        mbedtls_printf(HEADER_FORMAT, TITLE);                                                                         \
-        fflush(stdout);                                                                                               \
-        benchmark_mbedtls_set_alarm(3);                                                                               \
-                                                                                                                      \
-        ret = 0;                                                                                                      \
-        tsc = benchmark_mbedtls_timing_hardclock();                                                                   \
-        for (ii = 1; !benchmark_mbedtls_timing_alarmed && !ret; ii++)                                                 \
-        {                                                                                                             \
-            CODE;                                                                                                     \
-            benchmark_mbedtls_poll_alarm();                                                                           \
-        }                                                                                                             \
-                                                                                                                      \
-        if (ret != 0)                                                                                                 \
-        {                                                                                                             \
-            PRINT_ERROR;                                                                                              \
-        }                                                                                                             \
-        else                                                                                                          \
-        {                                                                                                             \
-            mbedtls_printf("%6.2f " TYPE "/s",                                                                        \
-                           ((float)ii) / ((benchmark_mbedtls_timing_hardclock() - tsc) / CLOCK_GetCoreSysClkFreq())); \
-            MEMORY_MEASURE_PRINT(sizeof(TYPE) + 1);                                                                   \
-            mbedtls_printf("\r\n");                                                                                   \
-        }                                                                                                             \
-    } while (0)
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-
-/*******************************************************************************
- * Variables
- ******************************************************************************/
 
 /*******************************************************************************
  * Code
@@ -315,17 +235,6 @@ void ecp_clear_precomputed( mbedtls_ecp_group *grp )
 
 unsigned char sign[signSIZE];
 
-typedef struct {
-    char md4, md5, ripemd160, sha1, sha256, sha512,
-         arc4, des3, des,
-         aes_cbc, aes_gcm, aes_ccm, aes_xts, chachapoly,
-         aes_cmac, des3_cmac,
-         aria, camellia, blowfish, chacha20,
-         poly1305,
-         havege, ctr_drbg, hmac_drbg,
-         rsa, dhm, ecdsa, ecdh;
-} todo_list;
-
 #if defined(FREESCALE_KSDK_BM) && !defined(MBEDTLS_TIMING_C)
 
 static volatile uint32_t s_MsCount = 0U;
@@ -371,101 +280,6 @@ static void benchmark_mbedtls_poll_alarm(void)
 }
 #endif
 
-static int bench_print_features(void)
-{
-    char *text;
-    mbedtls_printf("mbedTLS version %s\r\n", MBEDTLS_VERSION_STRING);
-    mbedtls_printf("fsys=%lu\r\n", ((CORE_CLK_FREQ)));
-    mbedtls_printf("Using following implementations:\r\n");
-#if defined(MBEDTLS_FREESCALE_LTC_SHA256)
-    text = "LTC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_MMCAU_SHA256)
-    text = "MMCAU HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_LPC_SHA256)
-    text = "LPC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAU3_SHA256)
-    text = "CAU3 HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_DCP_SHA256)
-    text = "DCP HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_HASHCRYPT_SHA256)
-    text = "HASHCRYPT HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAAM_SHA256)
-    text = "CAAM HW accelerated";
-#elif defined(MBEDTLS_NXP_SENTINEL200)
-    text = "S200 HW accelerated";
-#elif defined(MBEDTLS_NXP_SENTINEL300)
-    text = "S300 HW accelerated";
-#else
-    text = "Software implementation";
-#endif
-    mbedtls_printf("  SHA: %s\r\n", text);
-#if defined(MBEDTLS_FREESCALE_LTC_AES)
-    text = "LTC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_MMCAU_AES)
-    text = "MMCAU HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_LPC_AES)
-    text = "LPC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAU3_AES)
-    text = "CAU3 HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_DCP_AES)
-    text = "DCP HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_HASHCRYPT_AES)
-    text = "HASHCRYPT HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAAM_AES)
-    text = "CAAM HW accelerated";
-#elif defined(MBEDTLS_NXP_SENTINEL200)
-    text = "SW AES, S200 HW accelerated CCM and CMAC";
-#elif defined(MBEDTLS_NXP_SENTINEL300)
-    text = "SW AES, S300 HW accelerated CCM and CMAC";
-#else
-    text = "Software implementation";
-#endif
-    mbedtls_printf("  AES: %s\r\n", text);
-#if defined(MBEDTLS_FREESCALE_LTC_AES_GCM)
-    text = "LTC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_MMCAU_AES)
-    text = "MMCAU HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_LPC_AES_GCM)
-    text = "LPC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAU3_AES)
-    text = "CAU3 HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAAM_AES)
-    text = "CAAM HW accelerated";
-#else
-    text = "Software implementation";
-#endif
-    mbedtls_printf("  AES GCM: %s\r\n", text);
-#if defined(MBEDTLS_FREESCALE_LTC_DES)
-    text = "LTC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_MMCAU_DES)
-    text = "MMCAU HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAU3_DES)
-    text = "CAU3 HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAAM_DES)
-    text = "CAAM HW accelerated";
-#else
-    text = "Software implementation";
-#endif
-    mbedtls_printf("  DES: %s\r\n", text);
-#if defined(MBEDTLS_FREESCALE_LTC_PKHA)
-    text = "LTC HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CAU3_PKHA)
-    text = "CAU3 HW accelerated";
-#elif defined(MBEDTLS_FREESCALE_CASPER_PKHA)
-    text = "CASPER HW accelerated ECC256/384/521 and RSA verify";
-#elif defined(MBEDTLS_FREESCALE_CAAM_PKHA)
-    text = "CAAM HW accelerated";
-#elif defined(MBEDTLS_NXP_SENTINEL200)
-    text = "S200 HW accelerated ECDSA and ECDH";
-#elif defined(MBEDTLS_NXP_SENTINEL300)
-    text = "S300 HW accelerated ECDSA and ECDH";
-#else
-    text = "Software implementation";
-#endif
-    mbedtls_printf("  Asymmetric cryptography: %s\r\n", text);
-
-    return 0;
-}
 
 /* Keys and file management*/
 unsigned char *public_key =
@@ -526,9 +340,6 @@ static mbedtls_entropy_context entropy;
 int main( int argc, char *argv[] )
 {
     int i;
-    unsigned char tmp[200];
-    char title[TITLE_LEN];
-    todo_list todo;
 #if defined(MBEDTLS_MEMORY_signFER_ALLOC_C)
     unsigned char alloc_sign[HEAP_SIZE] = { 0 };
 #endif
@@ -544,7 +355,7 @@ int main( int argc, char *argv[] )
     /* call CMSIS SysTick function. It enables the SysTick interrupt at low priority */
     SysTick_Config(CLOCK_GetCoreSysClkFreq() / 1000U); /* 1 ms period */
 #endif
-    bench_print_features();
+
 
     /* Main Section */
     int ret = 0,v = 1;
@@ -555,16 +366,7 @@ int main( int argc, char *argv[] )
 
     unsigned char *test_message = "LGFErzqw+rC6O2zN3RNy79+eVVQ/NdAkq4tPbsvDHEaPPJcnIE1GSAoiEJ7Ppey5727SJRg7CGn4Zr6XBqRd5a6egINn3NGnKzuYnKurgnBKJukmoDXsA05Kevz4egXiLVVEOBg3e0BYWqmILGtk0BvLjv8RomiRktwChwiv25bocoLQ4mGRdXp9KKR0AYddNxSMIJx4Buf7qj6f/EmAc//yQrlT3tN3O9T2wFGgwu4r6R1VaviY7k5YAI2mInWztd38B9X6obkc84jiAMqHpHvTPtaevZ5OlrmWuAIzQYbJPkkOjm4mtBNmHMJ7eh/fR6W2ugrfqZDteNlQgSn+Mg==";
 
-    unsigned char *known_message = ",aD¯:°ú°º;lÍÝrïßUT?5Ð$«OnËÃF<' MFH\"Ï¥ì¹ïnÒ%;iøf¾¤]å®gÜÑ§+;««pJ&é& 5ìNJzüøzâ-UD87{@XZ©,kdÐËÿ¢hÜ¯ÛèrÐâauz}(¤t]7 xçûª>üIsÿòB¹SÞÓw;ÔöÀQ Âî+éUjøîNX¦\"u³µÝüÕú¡¹óâÊ¤{Ó>Ö½N¹¸3AÉ>In&´fÂ{zßG¥¶ºß©íxÙP)þ2";													      /* ^^^ We're missing here */
-
-    /*
-	unsigned char *sign = (unsigned char *)calloc(2048,sizeof(unsigned char));
-	unsigned char *sign_base64_encoded = (unsigned char *)calloc(2048,sizeof(unsigned char));
-	unsigned char *sign_base64_decoded = (unsigned char *)calloc(2048,sizeof(unsigned char));
-	unsigned char *signed_base64_encoded = (unsigned char *)calloc(2048,sizeof(unsigned char));
-	unsigned char *signed_base64_decoded = (unsigned char *)calloc(2048,sizeof(unsigned char));
-	unsigned char *hash_message = (unsigned char *)calloc(32,sizeof(unsigned char));
-	*/
+    unsigned char *known_message = ",aD¯:°ú°º;lÍÝrïßUT?5Ð$«OnËÃF<' MFH\"Ï¥ì¹ïnÒ%;iøf¾¤]å®gÜÑ§+;««pJ&é& 5ìNJzüøzâ-UD87{@XZ©,kdÐËÿ¢hÜ¯ÛèrÐâauz}(¤t]7 xçûª>üIsÿòB¹SÞÓw;ÔöÀQ Âî+éUjøîNX¦\"u³µÝüÕú¡¹óâÊ¤{Ó>Ö½N¹¸3AÉ>In&´fÂ{zßG¥¶ºß©íxÙP)þ2";
 
     /* Non dynamic so we can use sizeof() operators */
 	unsigned char sign[2048]={'\0'};
@@ -581,19 +383,9 @@ int main( int argc, char *argv[] )
 	mbedtls_pk_init( &pk_private );
 	mbedtls_entropy_init( &entropy );
 
-/* Quick Self Test */
-    if( ( ret = mbedtls_sha256_self_test( v ) ) != 0 )
-    {
-    	PRINTF("Sha256 self test failed!\n");
-        return( ret );
-    }
-    if( ( ret = mbedtls_base64_self_test( v ) ) != 0 )
-    {
-    	PRINTF("Base64 self test failed!\n");
-        return( ret );
-    }
 
 /* Testing base64 conversions */
+/*
     fflush( stdout );
 	if( ( ret = mbedtls_base64_encode( sign_base64_encoded, 2048, &olen,new_message,strlen(new_message)) ) != 0 )
 	{
@@ -604,14 +396,14 @@ int main( int argc, char *argv[] )
 
 	PRINTF( "\nRet=%d\nolen=%d\nBase64 Encoded Message:\n%s\n",ret,olen,sign_base64_encoded);
 	fflush( stdout );
-	if( ( ret = mbedtls_base64_decode( sign_base64_decoded, 2048, &olen,sign_base64_encoded,strlen(sign_base64_encoded)) ) != 0 )	//Isn't this size too big for RSA2048?
+	if( ( ret = mbedtls_base64_decode( sign_base64_decoded, 2048, &olen,sign_base64_encoded,strlen(sign_base64_encoded)) ) != 0 )
 	{
 		PRINTF( " \nfailed\nOlen = %d\nmbedtls_base64_encode returned -0x%04x\n", olen,-ret );
 		return -1;
 	}
 
 	PRINTF( "\nRet=%d\nolen=%d\nBase64 Decoded Message:\n%s\n",ret,olen,sign_base64_decoded);
-
+*/
 
 /* Setting Randomness Generator */
 	fflush( stdout );
@@ -690,6 +482,7 @@ int main( int argc, char *argv[] )
 	}
 
 	PRINTF("\n\nDecoded string... \n\n");
+	PRINTF("Base64 Encoded Message:\n");
 	for(i = 0; i < olen; i++) PRINTF("%c", signed_base64_decoded[i]);
     PRINTF("\n\n");
 
